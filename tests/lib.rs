7811b5428cdb4591b8bd9ef327f8f4fbfbfd143e
Mathias Danielsen <mathiasda98@gmail.com>
2021-06-29 00:01:57 +0200

    Use builtin rust testrunner to get cleaner output with fails and success

---
+++ tests/lib.rs
@@ -0,0 +1,43 @@
+use built_in_functions::declare_functions;
+
+fn run_file(file: &str) {
+    let mut interpreter = gray::load_file(file).unwrap();
+    declare_functions(&mut interpreter);
+    interpreter.run(None);
+}
+
+#[test]
+fn array_test() {
+    run_file("./tests/array_test.gray");
+}
+
+#[test]
+fn basic_test() {
+    run_file("./tests/basic_test.gray");
+}
+
+#[test]
+fn if_statement_test() {
+    run_file("./tests/if_statement_test.gray");
+}
+
+
+#[test]
+fn math_precedence_test() {
+    run_file("./tests/math_precedence_test.gray");
+}
+
+#[test]
+fn math_test() {
+    run_file("./tests/math_test.gray");
+}
+
+#[test]
+fn namespace_test() {
+    run_file("./tests/namespace_test.gray");
+}
+
+#[test]
+fn object_test() {
+    run_file("./tests/object_test.gray");
+}
